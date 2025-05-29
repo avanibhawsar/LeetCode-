390bb3f177517376d813addb51b3e0d834f8f3bf
Avani Bhawsar <164218648+avanibhawsar@users.noreply.github.com>
2025-05-29 23:56:28 +0530

    Create climbStairs70.cpp

---
+++ climbStairs70.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+ int find(int n){
+        int num1=0;
+        int num2=1;
+        int ans;
+        for(int i=1;i<=n;i++){
+            ans = num1 + num2;
+            num1=num2;
+            num2=ans;
+        }
+        return ans;
+    }
+
+    int climbStairs(int n) {
+    //     if(n==0)
+    //     {
+    //         return 0;
+    //     }
+    //     if(n==1)
+    //     {
+    //         return 1;
+    //     }
+    //    return climbStairs(n - 1) + climbStairs(n - 2);
+
+        return find(n);
+    }
+};

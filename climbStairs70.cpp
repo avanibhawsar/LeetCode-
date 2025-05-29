572f25d4eddb9db5c1c2f124586e84799211d350
Avani Bhawsar <164218648+avanibhawsar@users.noreply.github.com>
2025-05-29 23:56:56 +0530

    Update climbStairs70.cpp

--- climbStairs70.cpp
+++ climbStairs70.cpp
@@ -13,10 +13,7 @@ public:
     }
 
     int climbStairs(int n) {
-    //     if(n==0)
-    //     {
-    //         return 0;
-    //     }
+   
     //     if(n==1)
     //     {
     //         return 1;

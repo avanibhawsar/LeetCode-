669ba7214e46e90f546bcb47c7b634a542d3a5ab
Avani Bhawsar <164218648+avanibhawsar@users.noreply.github.com>
2025-06-03 10:28:24 +0530

    Create removeDuplicate26.cpp

---
+++ removeDuplicate26.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int i=0;
+        if(nums.empty())
+        {
+            return 0;
+        }
+        for(int j=0;j<nums.size();j++)
+        {
+            if(nums[j] != nums[i])
+            {
+                i++;
+                nums[i]=nums[j];
+            }
+        }
+
+        return i+1;
+    }
+};
